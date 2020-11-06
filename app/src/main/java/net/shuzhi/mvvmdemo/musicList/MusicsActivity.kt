26a1ae9a191e1d0ccb30c62b7482355cfffed63b
lsgdut <lhwywh@live.com>
2020-11-06 13:44:01 +0800

    feat(MusicPresenter):更新到29节课 LiveData另外一种添加观察者的方式监听数据变化

--- app/src/main/java/net/shuzhi/mvvmdemo/musicList/MusicsActivity.kt
+++ app/src/main/java/net/shuzhi/mvvmdemo/musicList/MusicsActivity.kt
@@ -6,12 +6,15 @@ import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_musics.*
 import net.shuzhi.mvvmdemo.R
 import net.shuzhi.mvvmdemo.base.BaseActivity
+import net.shuzhi.mvvmdemo.player.domain.Music
 
 /**
  * @author 梁爽
  * @create 2020/11/4 23:19
  */
 class MusicsActivity : BaseActivity() {
+
+    private lateinit var mForeverObserver:ForeverObserver
 
     private val musicPresenter by lazy { MusicPresenter(this) }
 
@@ -22,18 +25,34 @@ class MusicsActivity : BaseActivity() {
         initViewListeners()
     }
 
+    inner class ForeverObserver : Observer<List<Music>>{
+        override fun onChanged(result: List<Music>?) {
+            println("foreverObserver data change ---> ${result?.size}")
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        musicPresenter.liveMusicList.removeObserver(mForeverObserver)
+    }
+
+
     /**
      * 监听数据变化
      */
     private fun initDataListener() {
+        mForeverObserver = ForeverObserver()
+        musicPresenter.liveMusicList.observeForever(mForeverObserver)
 
-        musicPresenter.liveMusicList.observe(
-            this,
-            Observer {
-                println("live data里的音乐列表数据更新了${it?.size}条数据")
-                musicCountText?.text = "更新了${it?.size}条数据"
-            }
-        )
+//        musicPresenter.liveMusicList.observe(
+//            this,
+//            Observer {
+//                //去更新ui
+//                println("live data里的音乐列表数据更新了${it?.size}条数据")
+//                musicCountText?.text = "更新了${it?.size}条数据"
+//            }
+//        )
 
         musicPresenter.musicList.addListener(this) {
             println(Thread.currentThread().name)
